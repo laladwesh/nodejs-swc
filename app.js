7177ee636ec06b756fc89db26a9b729c3b1a658c
Avinash Gupta <167670888+laladwesh@users.noreply.github.com>
2024-06-25 14:04:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-// const express  = require('express');
-// const app = express();
-// app.listen(3000);
-// app.get('/' , (req,res) =>{
-//     res.sendFile('./index.html' , {root: __dirname})
-// });
-// app.get('/about' , (req,res) =>{
-//     res.sendFile('./about.html' , {root: __dirname})
-// });
-// app.get('/about-us', (req, res) => {
-//     res.redirect('/about');
-// });
-// app.use((req,res) =>{
-//     res.status(404).sendFile('./404.html' , {root: __dirname})
-// });
-// app.get('/', (req, res) => {
-//   res.sendFile('./index.html', { root: __dirname });
-// });
-// app.get('/about', (req, res) => {
-//   res.sendFile('./about.html', { root: __dirname });
-// });
-// app.get('/about-us', (req, res) => {
-//   res.redirect('/about');
-// });
-// app.use((req, res) => {
-//   res.status(404).sendFile('./404.html', { root: __dirname });
-// });
+
 const express  = require('express');
 const app = express();
 const path = require('path')
